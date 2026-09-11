fb614b94643a19f7bb563179e5ac5d3de51bacce
azazpaka <alexander.finley@ascte.org>
2026-09-11 11:24:12 -0500

    added name and removed bad stuff
    changed

--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 from goblin import Goblin
 
 
-ARENA_NAME = "The Iron Circle"
 ARENA_NAME = "Scary Larry's Domain"
 
 
@@ -9,12 +8,9 @@ def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
-    print("The gates are opening...")
     print("The door to Larry's mom's basement creaks open, and a foul stench fills the air.")
 
-    goblin = Goblin("Gribble")
-    goblin = Goblin("Scary Larry")
-
+    goblin = Goblin("Scary Le Terry")
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblin.name} is rotting on his computer with {goblin.health} health, and he lowk stink.")
     print("But no hero has answered the call... yet.")
